f49ef45608bdd5169a02b1e5ead755ada3b18165
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-03-17 03:25:38 +0530

    Started Building Lakeside Hotel

---
+++ Lakeside Server/src/test/java/com/amanbhardwaj/Lakeside/LakesideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amanbhardwaj.Lakeside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LakesideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
