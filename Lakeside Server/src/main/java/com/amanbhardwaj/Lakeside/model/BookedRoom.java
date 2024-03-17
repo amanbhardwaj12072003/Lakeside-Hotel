b0468fd50be66aa1a6c9311265c4e8f6ef215b5e
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-03-17 14:32:31 +0530

    Room and BookedRoom model ready

--- Lakeside Server/src/main/java/com/amanbhardwaj/Lakeside/model/BookedRoom.java
+++ Lakeside Server/src/main/java/com/amanbhardwaj/Lakeside/model/BookedRoom.java
@@ -1,4 +1,66 @@
 package com.amanbhardwaj.Lakeside.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_fullName")
+    private String guestFullName;
+
+    @Column(name = "guest_email")
+    private String guestEmail;
+
+    @Column(name = "num_adults")
+    private int numOfAdults;
+
+    @Column(name = "num_children")
+    private int numOfChildren;
+
+    @Column(name = "total_guest")
+    private int totalNumOfGuest;
+
+    @Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+
+    @JoinColumn(name = "room_id")
+    @ManyToOne(fetch  = FetchType.LAZY)
+    private Room room;
+
+    public void calculateTotalNumberOfGuest(){
+        this.totalNumOfGuest = this.numOfAdults + this.numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
--- Lakeside Server/src/main/java/com/amanbhardwaj/Lakeside/model/Room.java
+++ Lakeside Server/src/main/java/com/amanbhardwaj/Lakeside/model/Room.java
@@ -1,4 +1,29 @@
 package com.amanbhardwaj.Lakeside.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomId;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked = false;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    public Room(){
+        this.bookings = new ArrayList<>();
+    }
 }
