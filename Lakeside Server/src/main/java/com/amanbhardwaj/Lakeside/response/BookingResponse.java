719901e1a8e85c60f23049ccc8111b0fbab19c23
Aman Bhardwaj <amanbhardwaj12072003@gmail.com>
2024-03-18 13:06:41 +0530

    Application.yml configured

--- Lakeside Server/src/main/java/com/amanbhardwaj/Lakeside/response/BookingResponse.java
+++ Lakeside Server/src/main/java/com/amanbhardwaj/Lakeside/response/BookingResponse.java
@@ -1,5 +1,31 @@
 package com.amanbhardwaj.Lakeside.response;
 
-public class BookingResponse {
+import com.amanbhardwaj.Lakeside.model.Room;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingResponse {
+    private Long bookingId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+    private Room room;
+
+    public BookingResponse(Long bookingId, LocalDate checkInDate, LocalDate checkOutDate, String bookingConfirmationCode) {
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
